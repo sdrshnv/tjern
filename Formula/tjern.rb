1a9da6afa1056acd3dab4ff25b3cfaadb7e982e0
sudarshanvankudre <sudarshansvankudre@gmail.com>
2024-04-14 17:58:05 -0700

    Working brew formula

--- Formula/tjern.rb
+++ Formula/tjern.rb
@@ -1,22 +1,17 @@
-require "language/go"
-
 class Tjern < Formula
-  desc "An end-to-end encrypted terminal based application for journaling"
+  desc "A happy little journaling app"
   homepage "https://github.com/sudarshanvankudre/tjern"
   url "https://github.com/sudarshanvankudre/tjern/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "dc657dee4e2db0b928047403c50b0fcc6ba5c378a94c8894483e1f6757bd049f"
-  license "GPL-3.0-only"
-  head "https://github.com/sudarshanvankudre/tjern.git"
+  license "GPL-3.0"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    system "go", "build", *std_go_args
-    bin.install "tjern"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
     system "true"
   end
-end 
+end
