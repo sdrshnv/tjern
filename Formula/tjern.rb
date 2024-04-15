49cd0c83eaa5e32537c11f912d594d63bb727237
sudarshan <sudarshansvankudre@gmail.com>
2024-04-14 23:08:24 -0700

    Update tjern.rb

--- Formula/tjern.rb
+++ Formula/tjern.rb
@@ -2,7 +2,7 @@ class Tjern < Formula
   desc "A happy little journaling app"
   homepage "https://github.com/sudarshanvankudre/tjern"
   url "https://github.com/sudarshanvankudre/tjern/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "dc657dee4e2db0b928047403c50b0fcc6ba5c378a94c8894483e1f6757bd049f"
+  sha256 "8b9cf04f1fb3c0639076118caf4a3e0c81f4aa2ff468f10f14ec85a5d7def280"
   license "GPL-3.0"
 
   depends_on "go" => :build
