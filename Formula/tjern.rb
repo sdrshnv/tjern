aa95d778d0994c05fed187305fdff395c84ec2ed
sudarshanvankudre <sudarshansvankudre@gmail.com>
2024-05-19 23:52:25 -0700

    Update formula

--- Formula/tjern.rb
+++ Formula/tjern.rb
@@ -1,7 +1,7 @@
 class Tjern < Formula
   desc "A happy little journaling app"
   homepage "https://github.com/sdrshnv/tjern"
-  url "https://github.com/sdrshnv/tjern/archive/refs/tags/v0.7.1.tar.gz"
+  url "https://github.com/sdrshnv/tjern/archive/refs/tags/v0.7.2.tar.gz"
   sha256 "4c0815a8da50c939afa5833cc371e64c636ae3d18872ac36996431ed5e30aec4"
   license "GPL-3.0"
 
